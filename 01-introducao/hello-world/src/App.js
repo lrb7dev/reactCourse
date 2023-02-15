d9a46aa03193edd7999f152df29ea2837d24c100
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2023-02-15 18:55:00 -0300

    01-intro

---
+++ 01-introducao/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="color-1">Hello World REACT!</h1>
+      <h2>Tarefa 01</h2>
+      <p className="color-2">Esse é um texto de teste, apenas para ilustrar a tarefa!</p>
+    </div>
+  );
+}
+
+export default App;
