c517e6a69d912b24c50fba80ffef79069ddc6557
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2023-02-17 16:45:37 -0300

    02-fundamentos

---
+++ 02-fundamentos/fundamentos/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FirstCompoent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>FUNDAMENTOS</h1>
+      {/*First component --- comment */}
+      <FirstCompoent></FirstCompoent>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-fundamentos/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,15 @@
+//creation of the first component
+const FirstCompoent = () => {
+    //about comments: outside of jsx, that's the form to comment
+
+    return (
+        //comment
+        <div>
+            {/**about comments: inside of jsx, that's the form to comment */}    
+            {/** correct comment form */}
+            <h2>My first component!</h2>
+        </div>
+    )    
+}
+
+export default FirstCompoent
