9c32eba32b8f1767caf3c26aeefcc96bde8958cc
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2023-02-22 15:22:51 -0300

    New

--- 03-avançando em react/sessao03/src/App.js
+++ 03-avançando em react/sessao03/src/App.js
@@ -1,12 +1,16 @@
 
+import { useState } from 'react';
 import './App.css';
 
 import Jazz from "./assets/jazz.jpg"
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import TrabalhandoComProps from './components/TrabalhandoComProps';
 
 function App() {
+  const nome = "Pedro"
+  const [ultimoNome] = useState("Alcantara")
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -21,7 +25,9 @@ function App() {
       {/* <ManageData></ManageData>
       <ListRender></ListRender>   */}
       {/* conditional render */}
-      <ConditionalRender/>
+      {/* <ConditionalRender/> */}
+      {/* Formas de se passar as props */}
+      <TrabalhandoComProps name={nome} lastName={ultimoNome} age={30} />
     </div>
   );
 }
---
+++ 03-avançando em react/sessao03/src/components/TrabalhandoComProps.js
@@ -0,0 +1,10 @@
+const TrabalhandoComProps = (props) => {
+  return (
+    <div>
+        <h1>Trabalhando Com Props</h1>
+        <h2>O nome do usuário: {props.name} {props.lastName} de {props.age} anos</h2>
+    </div>
+  )
+}
+
+export default TrabalhandoComProps
