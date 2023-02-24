97e8c91424e4be4697d13234c04df1613e9dfda2
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2023-02-24 11:50:43 -0300

    new

---
+++ 05-form-em-react/forms/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MyForm from './components/MyForm';
+
+function App() {
+  return (
+    <div className="App">
+      {/*  1 - criacao de form */}
+      <MyForm/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
