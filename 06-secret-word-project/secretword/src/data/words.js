79c690644c793411f9edecc008f0858bf0e6aee9
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2023-02-26 10:15:41 -0300

    creating the project

---
+++ 06-secret-word-project/secretword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Motor", "Roda", "Engrenagem", "Volante", "Suspensão", "Câmbio"],
+    fruta: ["Maçã", "Melão", "Banana", "Abacaxi", "Goiaba", "Amora"],
+    corpo: ["Perna", "Cabeça", "Pulmão", "Coração", "Mãos", "Pelve"],
+    computador: ["Gabinete", "Teclado", "Mouse", "Monitor"],
+    programação: ["Linguagem", "Framework", "Javascript", "React", "Funções", "Arrays"],
+    alimento: ["Arroz", "Feijão", "Fritas", "Carne", "Salada", "Hamburguer", "Sopa"]
+}
