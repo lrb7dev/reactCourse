ed3e65416568cb2207897d8e8c28761b7be51df9
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2023-04-18 22:03:02 -0300

    criando Provider

--- 09-contextAPI/context/src/index.js
+++ 09-contextAPI/context/src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { CounterContextProvider } from './context/CounterContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  {/* 2 - criando Provider */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
+    
   </React.StrictMode>
 );
 
